1342e90ab8fd6fc291504d21c964f4f7d11f1c79
shuflov <shuflovic@gmail.com>
2026-03-05 17:04:33 +0100

    Fix repo name for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bookshelf_AI/' // IMPORTANT: Set this to your repository name
+  base: '/bookshelf/' // IMPORTANT: Set this to your repository name
 })
