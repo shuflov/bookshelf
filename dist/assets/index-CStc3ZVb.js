ff6b7594a318e6fa1a5c895f73f349f2c99c5c17
shuflovic <pavel.skuta@gmail.com>
2026-03-22 21:21:34 +0100

    Fix base path for localhost (use / instead of /bookshelf/)

--- dist/assets/index-CStc3ZVb.js
+++ dist/assets/index-CStc3ZVb.js
@@ -12105,7 +12105,7 @@ const ActionButton = ({ onClick, text, primary = false, disabled = false }) => {
 };
 const scriptRel = "modulepreload";
 const assetsURL = function(dep) {
-  return "/bookshelf/" + dep;
+  return "/" + dep;
 };
 const seen = {};
 const __vitePreload = function preload2(baseModule, deps, importerUrl) {
@@ -37173,4 +37173,4 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   /* @__PURE__ */ jsxRuntimeExports.jsx(React$2.StrictMode, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, {}) })
 );
-//# sourceMappingURL=index-CLoySzS_.js.map
+//# sourceMappingURL=index-CStc3ZVb.js.map
